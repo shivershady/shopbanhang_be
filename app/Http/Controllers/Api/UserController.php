38b3d70eb62e76f4b76c8fbf3c6ab60189940d12
Miên <2502mien@gmail.com>
2021-10-09 22:51:34 +0700

    update register

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,47 +8,50 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use PHPUnit\Util\Exception;
+
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-
-        $request->validate([
-            'name' => 'required|max:50',
-            'email' => 'required:email',
-            'password' => 'required|min:6'
-           // 'c_password' => 'required:same:password'
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
         ]);
 
-        try {
-            $data = request()->only(['name', 'email', 'password']);
-            $data['password'] = Hash::make($data['password']);
-            $data['email_verified_at'] = Carbon::now();
-            User::create($data);
-            //GỬI EMAIL MÃ OTP VỀ ĐỊA CHỈ MAIL NGƯỜI DÙNG
-//            $random = Str::random(6);
-            //gọi đến gửi mail
-        } catch (Exception $e) {
-            return response()->json('error', 500);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
         }
+
+        $data = request()->only(['name', 'email', 'password']);
+        $data['password'] = Hash::make($data['password']);
+        $data['email_verified_at'] = Carbon::now();
+        User::create($data);
+
         return response()->json(['success', 'đăng ký thành công'], 200);
+
+        //GỬI EMAIL MÃ OTP VỀ ĐỊA CHỈ MAIL NGƯỜI DÙNG
+//             $random = Str::random(6);
+        //gọi đến gửi mail
     }
 
-  /*  public function resendOTP(Request $request)
-    {
-        //gửi lại OTP dựa theo email
-    }*/
+    /*  public function resendOTP(Request $request)
+      {
+          //gửi lại OTP dựa theo email
+      }*/
 
 
-  /*  public function verifyEmail(Request $request)
-    {
-        $email = $request->input('email');
-        $otp = $request->input('otp');
-        return response()->json(['success', 'Xác thực email thành công'], 200);
-    }*/
+    /*  public function verifyEmail(Request $request)
+      {
+          $email = $request->input('email');
+          $otp = $request->input('otp');
+          return response()->json(['success', 'Xác thực email thành công'], 200);
+      }*/
 
 
     public function login(Request $request)
